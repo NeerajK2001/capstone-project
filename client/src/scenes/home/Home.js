65a4a6ac61ec80cfca1b35e6e31600af41dfffe6
jaskaran preet singh <90711010+jsingh189@users.noreply.github.com>
2023-04-02 01:39:22 -0600

    small update

--- client/src/scenes/home/Home.js
+++ client/src/scenes/home/Home.js
@@ -57,6 +57,7 @@ function Home() {
 
       <div className="section-6"></div>
         <div className="section-4">
+          <div className="sec4-overlay"></div>
           <div className="about">
             <h3><span>AB</span>OUT US</h3>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
